dd856c7aa4eb10b5693266ec2a73786c1b194e1c
james9001 <james9001@james9001.net>
2025-02-07 16:54:39 +1100

    ESLint rule which allows unused variables as args, vars and caught errors - IF they start with underscore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,19 @@ module.exports = {
 		jsx: false,
 	},
 	plugins: ["@typescript-eslint"],
-	rules: {},
+	rules: {
+		// note you must disable the base rule
+		// as it can report incorrect errors
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{
+				argsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+				caughtErrorsIgnorePattern: "^_",
+			},
+		],
+	},
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/eslint-recommended",
